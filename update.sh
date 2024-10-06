42f5b1e86a9901b463d96ff759414d9da28d5cbd
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-10-05 22:01:57 -0500

    el script ahora usa la branch 1-deploy que tiene las urls con el nombre de los contenedores en la network

--- update.sh
+++ update.sh
@@ -5,9 +5,9 @@ rm -rf Microservicio-Cliente
 rm -rf Microservicio-Movimientos
 rm -rf Microservicio-Promociones
 
-git clone https://github.com/CS2032-Proyecto-Final/Microservicio-Promociones.git
-git clone https://github.com/CS2032-Proyecto-Final/Microservicio-Movimientos.git
-git clone https://github.com/CS2032-Proyecto-Final/Microservicio-Cliente.git
-git clone https://github.com/CS2032-Proyecto-Final/Microservicio-Orquestador.git
+git clone -b 1-deploy https://github.com/CS2032-Proyecto-Final/Microservicio-Promociones.git
+git clone -b 1-deploy https://github.com/CS2032-Proyecto-Final/Microservicio-Movimientos.git
+git clone -b 1-deploy https://github.com/CS2032-Proyecto-Final/Microservicio-Cliente.git
+git clone -b 1-deploy https://github.com/CS2032-Proyecto-Final/Microservicio-Orquestador.git
 
 docker compose build
